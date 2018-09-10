168584d1a60336f1d29a6f503dbbdd7d9ac5e3df
Haakon Bakker <hba@it-minds.no>
2018-09-10 12:05:49 +0200

    Add GetLetterGrade to RankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -2,12 +2,33 @@
 using System.Collections.Generic;
 using System.Text;
 using GradeBook.Enums;
+using System.Linq;
 
 namespace GradeBook.GradeBooks {
 	public class RankedGradeBook : BaseGradeBook {
 		public RankedGradeBook(string name) : base(name) {
-			Name = name;
 			Type = GradeBookType.Ranked;
+		}
+
+		public override char GetLetterGrade(double averageGrade) {
+			if(base.Students.Count < 5) throw new InvalidOperationException("Less than 5 students");
+
+			var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+			var grades = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();
+			
+			if(grades[threshold - 1] < averageGrade) {
+				return 'A';
+			}
+			else if(grades[threshold*2 - 1] < averageGrade) {
+				return 'B';
+			}
+			else if(grades[threshold*3 - 1 ] < averageGrade) {
+				return 'C';
+			}
+			else if (grades[threshold * 3 - 1] < averageGrade) {
+				return 'C';
+			}
+			return 'F';
 		}
 	}
 }
--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -6,7 +6,6 @@ using GradeBook.Enums;
 namespace GradeBook.GradeBooks {
 	public class StandardGradeBook : BaseGradeBook{
 		public StandardGradeBook(string name) : base(name) {
-			Name = name;
 			Type = GradeBookType.Standard;
 		}
 	}
