507b3af248a6c4b84a42855316caf1ea2eea156f
Haakon Bakker <hba@it-minds.no>
2018-09-10 13:47:32 +0200

    Add support for weighted grading

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -5,7 +5,7 @@ using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks {
 	public class StandardGradeBook : BaseGradeBook{
-		public StandardGradeBook(string name) : base(name) {
+		public StandardGradeBook(string name, bool isWeighted) : base(name, isWeighted) {
 			Type = GradeBookType.Standard;
 		}
 	}
